1f48d875ff917027c103e6a092fe09fbfa01bac5
Daniel Bültmann <dbn@comnets.rwth-aachen.de>
2008-01-18 17:51:03 +0100

    Initial import

---
+++ config.py
@@ -0,0 +1,285 @@
+import random
+random.seed(22)
+
+import dll
+
+import wns
+import wns.WNS
+import wns.Logger
+from wns import dB, dBm, fromdB, fromdBm
+from wns.Interval import Interval
+
+import constanze.Constanze
+import constanze.Node
+
+import wifimac.support.NodeCreator
+import wifimac.ProbeBus
+import wifimac.PathSelection
+
+import ofdmaphy.OFDMAPhy
+
+import rise.Scenario
+
+import speetcl.probes.AccessList
+
+from ip.VirtualARP import VirtualARPServer
+from ip.VirtualDHCP import VirtualDHCPServer
+from ip.VirtualDNS import VirtualDNSServer
+
+#######################
+# Simulation parameters
+#
+# Simulation of the string topology: all nodes are placed equidistantly
+# on a string, on each end of the string, an AP is positioned
+# Traffic is either DL only or bidirectional
+#
+simTime = 5.5
+commonLoggerLevel = 1
+dllLoggerLevel = 2
+
+# length of the string
+numMPs = 2
+numSTAs = 4
+distanceBetweenMPs = 50
+verticalDistanceSTAandMP = 10
+
+# load
+meanPacketSize = 1480 * 8
+offeredDL = 0.5e6
+offeredUL = 0.25e6
+ulIsActive = True
+startDelayUL = 1.01
+startDelayDL = 1.02
+
+# Available frequencies for bss and backbone, in MHz
+meshFrequencies = [5500]
+bssFrequencies = [2400, 2440, 2480]
+# End simulation parameters
+###########################
+
+# create an instance of the WNS configuration
+# The variable must be called WNS!!!!
+WNS = wns.WNS.WNS()
+WNS.outputStrategy = wns.WNS.OutputStrategy.DELETE
+WNS.maxSimTime = simTime
+WNS.PDataBase.settlingTime = 3
+#WNS.masterLogger.backtrace.enabled = True
+#WNS.masterLogger.enabled = True
+WNS.statusWriteInterval = 120 # in seconds realTime
+WNS.probesWriteInterval = 3600 # in seconds realTime
+
+#################
+# Create scenario
+scenarioXSize = distanceBetweenMPs*(numMPs + 2)
+scenarioYSize = verticalDistanceSTAandMP
+
+riseConfig = WNS.modules.rise
+riseConfig.debug.transmitter = (commonLoggerLevel > 1)
+riseConfig.debug.receiver    = (commonLoggerLevel > 1)
+riseConfig.debug.main = (commonLoggerLevel > 1)
+
+ofdmaPhyConfig = WNS.modules.ofdmaPhy
+managerPool = wifimac.support.NodeCreator.ManagerPool(xSize = scenarioXSize,
+                                                      ySize = scenarioYSize,
+                                                      maxNumRadios = len(meshFrequencies)+1,
+                                                      ofdmaPhyConfig = ofdmaPhyConfig)
+# End create scenario
+#####################
+
+######################################
+# Radio channel propagation parameters
+myPathloss = rise.scenario.Pathloss.PyFunction(validFrequencies = Interval(2000, 6000),
+                                               validDistances = Interval(2, 5000), # [m]
+                                               offset = dB(-27.552219),
+                                               freqFactor = 20,
+                                               distFactor = 35,
+                                               distanceUnit = "m", # nur fuer die Formel, nicht fuer validDistances
+                                               minPathloss = dB(42), # pathloss at 2m distance
+                                               outOfMinRange = rise.scenario.Pathloss.Constant("42 dB"), #Pathloss.FreeSpace(),
+                                               outOfMaxRange = rise.scenario.Pathloss.Deny(),
+                                               scenarioWrap = False,
+                                               sizeX = scenarioXSize,
+                                               sizeY = scenarioYSize)
+#myShadowing = rise.scenario.Shadowing.SpatialCorrelated(shadowSigma=5, correlationDistance=50)
+myShadowing = rise.scenario.Shadowing.No()
+myFastFading = rise.scenario.FastFading.No()
+propagationConfig = rise.scenario.Propagation.Configuration(pathloss = myPathloss, shadowing = myShadowing, fastFading = myFastFading)
+# End radio channel propagation parameters
+##########################################
+
+###################################
+#Create nodes using the NodeCreator
+nc = wifimac.support.NodeCreator.NodeCreator(propagationConfig)
+
+# one RANG
+rang = nc.createRANG()
+rang.logger.level = dllLoggerLevel
+if(ulIsActive):
+    # The RANG only has one IPListenerBinding that is attached
+    # to the listener. The listener is the only traffic sink
+    # within the RANG
+    ipListenerBinding = constanze.Node.IPListenerBinding(rang.nl.domainName, parentLogger=rang.logger)
+    listener = constanze.Node.Listener(rang.nl.domainName + ".listener", probeWindow = 0.1, parentLogger=rang.logger)
+    rang.load.addListener(ipListenerBinding, listener)
+    rang.nl.windowedEndToEndProbe.config.windowSize = 2.0
+    rang.nl.windowedEndToEndProbe.config.sampleInterval = 0.5
+WNS.nodes.append(rang)
+
+# create (magic) service nodes
+# One virtual ARP Zone
+varp = VirtualARPServer("VARP", "theOnlyZone")
+varp.logger.level = commonLoggerLevel
+WNS.nodes.append(varp)
+
+# One virtual DNS server
+vdns = VirtualDNSServer("VDNS", "ip.DEFAULT.GLOBAL")
+vdns.logger.level = commonLoggerLevel
+WNS.nodes.append(vdns)
+
+# One virtual pathselection server
+vps = wifimac.PathSelection.VirtualPSServer("VPS", numNodes = (numSTAs + (numMPs+2)*(len(meshFrequencies)+1) + 1))
+vps.logger.level = dllLoggerLevel
+WNS.nodes.append(vps)
+
+# Single instance of id-generator for all nodes with ids
+idGen = wifimac.support.NodeCreator.idGenerator()
+
+# save IDs for probes
+apIDs = []
+mpIDs = []
+staIDs = []
+apAdrs = []
+mpAdrs = []
+
+# selection of the BSS-frequency: iterating over the BSS-set
+bssCount = 0
+
+# One AP at the beginning
+f = [bssFrequencies[bssCount % len(bssFrequencies)]]
+f.extend(meshFrequencies)
+ap = nc.createAP(idGen = idGen,
+              	 position = wns.Position(distanceBetweenMPs/2, 0, 0),
+                 beaconDelay = 0.001,
+                 managerPool = managerPool,
+                 frequencies = f)
+ap.logger.level = commonLoggerLevel
+ap.dll.logger.level = dllLoggerLevel
+WNS.nodes.append(ap)
+apIDs.append(ap.id)
+apAdrs.extend(ap.dll.addresses)
+rang.dll.addAP(ap)
+print "Created AP at (", distanceBetweenMPs/2, ", 0, 0) with id ", ap.id, " and addresses ", ap.dll.addresses
+
+# Create MPs
+for i in xrange(numMPs):
+    bssCount+=1
+    f = [bssFrequencies[bssCount % len(bssFrequencies)]]
+    f.extend(meshFrequencies)
+    mp = nc.createMP(idGen = idGen,
+                     position = wns.Position(distanceBetweenMPs/2+distanceBetweenMPs*(i+1), 0, 0),
+                     beaconDelay = 0.001*(i+2),
+                     managerPool = managerPool,
+                     frequencies = f)
+    mp.logger.level = commonLoggerLevel
+    mp.dll.logger.level = dllLoggerLevel
+    WNS.nodes.append(mp)
+    mpIDs.append(mp.id)
+    mpAdrs.extend(mp.dll.addresses)
+    print "Created MP at (", distanceBetweenMPs/2+distanceBetweenMPs*(i+1), ", 0, 0) with id ", mp.id, " and addresses ", mp.dll.addresses
+
+# Create Last AP at the end
+bssCount+=1
+f = [bssFrequencies[bssCount % len(bssFrequencies)]]
+f.extend(meshFrequencies)
+ap = nc.createAP(idGen = idGen,
+                 position = wns.Position(distanceBetweenMPs/2+distanceBetweenMPs*(numMPs+1), 0, 0),
+                 beaconDelay = 0.001*(numMPs+2),
+                 managerPool = managerPool,
+                 frequencies = f)
+ap.logger.level = commonLoggerLevel
+ap.dll.logger.level = dllLoggerLevel
+WNS.nodes.append(ap)
+apIDs.append(ap.id)
+apAdrs.extend(ap.dll.addresses)
+rang.dll.addAP(ap)
+print "Created AP at (", distanceBetweenMPs/2+distanceBetweenMPs*(numMPs+1), ", 0, 0) with id ", ap.id, " and addresses ", ap.dll.addresses
+
+# Create STAs in equidistance spread out over scenario
+staDist = scenarioXSize/(numSTAs-1)
+for j in xrange(numSTAs):
+    sta = nc.createSTA(idGen, wns.Position(staDist*j,verticalDistanceSTAandMP,0), managerPool, bssFrequencies)
+    sta.logger.level = commonLoggerLevel
+    sta.dll.logger.level = dllLoggerLevel
+    print "Created STA at (", staDist*j, ", ", verticalDistanceSTAandMP, ", 0) with id ", sta.id
+
+    # DL load RANG->STA
+    cbrDL = constanze.Constanze.CBR(startDelayDL+random.random()*0.001, offeredDL, meanPacketSize, parentLogger=rang.logger)
+    ipBinding = constanze.Node.IPBinding(rang.nl.domainName, sta.nl.domainName, parentLogger=rang.logger)
+    rang.load.addTraffic(ipBinding, cbrDL)
+
+    # Listener at STA for DL
+    ipListenerBinding = constanze.Node.IPListenerBinding(sta.nl.domainName, parentLogger=sta.logger)
+    listener = constanze.Node.Listener(sta.nl.domainName + ".listener", probeWindow = 0.1, parentLogger=sta.logger)
+    sta.load.addListener(ipListenerBinding, listener)
+
+    if(ulIsActive):
+        # UL load STA->RANG
+        cbrUL = constanze.Constanze.CBR(startDelayUL+random.random()*0.001, offeredUL, meanPacketSize, parentLogger=sta.logger)
+        ipBinding = constanze.Node.IPBinding(sta.nl.domainName, rang.nl.domainName, parentLogger=sta.logger)
+        sta.load.addTraffic(ipBinding, cbrUL)
+
+    # IP Route Table
+    sta.nl.addRoute("192.168.1.0", "255.255.255.0", "0.0.0.0", "wifi")
+    sta.nl.addRoute(rang.nl.dataLinkLayers[0].addressResolver.address,
+                    "255.255.255.255",
+                    rang.nl.dataLinkLayers[0].addressResolver.address,
+                    "wifi")
+    rang.nl.addRoute(sta.nl.dataLinkLayers[0].addressResolver.address,
+                     "255.255.255.255",
+                     sta.nl.dataLinkLayers[0].addressResolver.address,
+                     "wifi")
+
+    # Add STA
+    WNS.nodes.append(sta)
+    staIDs.append(id)
+# End create nodes
+##################
+
+
+########################
+# Probing the simulation
+
+# IP: Old-school probing, generate dict, accessList and restrict probes
+# IP-Probes are only generated for the RANG -> incomming for uplink, aggregated for downlink
+nodeAccessList = speetcl.probes.AccessList.AccessList('wns.node.Node.id')
+nodeAccessList.addRange(min=1, max=1)
+
+settings = {}
+for dist in ['hop', 'endToEnd'] :
+    settings['ip.' + dist + '.packet.incoming.delay'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 1.0, 'resolution' : 1000}
+    settings['ip.' + dist + '.packet.outgoing.delay'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 1.0, 'resolution' : 1000}
+    settings['ip.' + dist + '.packet.incoming.size'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 16000.0, 'resolution' : 2000}
+    settings['ip.' + dist + '.packet.outgoing.size'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 16000.0, 'resolution' : 2000}
+    settings['ip.' + dist + '.packet.incoming.bitThroughput'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 10E6, 'resolution' : 10000}
+
+    for direction in ['incoming', 'outgoing', 'aggregated'] :
+        settings['ip.' + dist + ".window." + direction + '.bitThroughput'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 100E6, 'resolution' : 10000}
+        settings['ip.' + dist + ".window." + direction + '.compoundThroughput'] = {'sorting' : nodeAccessList, 'minXValue' : 0.0, 'maxXValue' : 10000.0, 'resolution' : 1000}
+
+statevals = speetcl.probes.ProbeHelpers.buildSortPDFEvalFromDict(settings)
+WNS.modules.ip.probes = speetcl.probes.ProbeFactory.withSortingCriterionByDict(statevals)
+
+# suppress output of IP but throughput probes for crosscheck with WiFiMac Probes
+for (k,v) in WNS.modules.ip.probes.items():
+    if (k.find('ip.endToEnd.') >= 0):
+        continue
+    speetcl.probes.ProbeModding.doIgnore(v)
+
+# Suppress all Constanze Probes
+WNS.modules.constanze.probes.clear()
+
+# Now the real thing: wifimac probes
+# Deactivated probes: system-test seems not to recognize probes by probe-bus?
+##probeBusses = wifimac.ProbeBus.getProbeBusses(apIDs, mpIDs, staIDs, apAdrs, mpAdrs, staIDs, maxHopCount = numMPs+1, performanceProbes = True, networkProbes = False)
+##for subtree in probeBusses.values():
+##    WNS.probeBusses.insertSubTree(subtree)
---
+++ systemTest.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python2.4
+
+# this is needed, so that the script can be called from everywhere
+import os
+import sys
+base, tail = os.path.split(sys.argv[0])
+os.chdir(base)
+
+# Append the python sub-dir of WNS--main--x.y ...
+sys.path.append(os.path.join('..', '..', '..', 'sandbox', 'default', 'lib', 'python2.4', 'site-packages'))
+
+# ... because the module WNS unit test framework is located there.
+import pywns.WNSUnit
+
+testSuite = pywns.WNSUnit.TestSuite()
+
+# create a system test
+testSuite.addTest(pywns.WNSUnit.ProbesTestSuite(sandboxPath = os.path.join('..', '..', '..', 'sandbox'),
+                                                executeable = "wns-core",
+                                                configFile = 'config.py',
+                                                runSimulations = True,
+                                                shortDescription = '2APs/2MPs network with 4 STAs',
+                                                disabled = False,
+                                                disabledReason = ""))
+
+if __name__ == '__main__':
+    # This is only evaluated if the script is called by hand
+
+    # if you need to change the verbosity do it here
+    verbosity = 2
+
+    pywns.WNSUnit.verbosity = verbosity
+
+    # Create test runner
+    testRunner = pywns.WNSUnit.TextTestRunner(verbosity=verbosity)
+
+    # Finally, run the tests.
+    testRunner.run(testSuite)
